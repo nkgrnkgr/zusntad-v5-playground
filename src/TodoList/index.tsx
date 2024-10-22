24ecbbeae69d4a1752ccc75d4769fc0b1a5e24ab
nkgrnkgr <beefupme@gmail.com>
2024-10-22 09:40:56 +0900

    update

--- src/TodoList/index.tsx
+++ src/TodoList/index.tsx
@@ -4,6 +4,9 @@ import { SearchKeyword } from "./SearchKeyword";
 
 export function TodoList() {
   // TODO store　の中から name に searchKeyword が含まれるものだけ表示する Selector を作成してください
+  // [参考]
+  // - https://kakehashi-dev.hatenablog.com/entry/2024/10/20/185225
+  // - https://github.com/reduxjs/reselect
   const ids = ["1"];
 
   return (
